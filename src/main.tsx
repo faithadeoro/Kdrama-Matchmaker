92dc1e8973156e2e257b372001835d15fc9f8020
faithadeoro <faithadeoro@gmail.com>
2025-11-27 13:11:15 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,3 +10,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 ) 
+export default {
+  port: 3000,
+  fetch(req: Request) {
+    if (new URL(req.url).pathname === "/") {
+      return new Response("The server running!");
+    }
+    return new Response("Not Found", { status: 404 });
+  }
+}
